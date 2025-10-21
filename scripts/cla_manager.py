b2992098e01b013f002048183c94604535030ca3
normansden <159940766+normansden@users.noreply.github.com>
2025-10-21 00:37:33 -0700

    Update cla_manager.py

--- scripts/cla_manager.py
+++ scripts/cla_manager.py
@@ -18,7 +18,7 @@ import urllib.request
 from typing import Dict, List, Optional, Tuple
 
 # ----------------------------- Config -----------------------------------------
-TARGET_STUB_VERSION = "10"
+TARGET_STUB_VERSION = "11"
 STUB_PATH = ".github/workflows/cla-check-trigger.yml"
 WORK_BRANCH = "automation/cla-stub"
 DEFAULT_EXCLUDES = [".github", ".github-*", "security", "security-*", "admin", "admin-*"]
